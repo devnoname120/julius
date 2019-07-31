ab68e0cb0ee2e9f0ee2d3de4a895bf7368681e80
devnoname120 <devnoname120+gh@gmail.com>
2019-07-31 16:35:27 +0200

    Add missing dependencies in Ubuntu Trusty

--- .ci_scripts/start_docker.sh
+++ .ci_scripts/start_docker.sh
@@ -5,6 +5,8 @@ case "$BUILD_TARGET" in
     # At the time of writing this code this image corresponds to Ubuntu Xenial (16.04 LTS)
     # The idea of AppImage is that you have to build on the oldest distribution that you want to support
 	docker run -d --name appimage --device /dev/fuse:mrw --workdir /build/git -v "${PWD}:/build/git" ubuntu:xenial tail -f /dev/null
+	# This image doesn't have cmake, make, etc.
+	docker exec appimage /bin/bash -c "apt install build-essential cmake"
 	;;
 "vita")
 	docker run -d --name vitasdk --workdir /build/git -v "${PWD}:/build/git" gnuton/vitasdk-docker tail -f /dev/null
