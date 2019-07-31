625b0bafebfa4472421e243880a24090a83c423d
devnoname120 <devnoname120+gh@gmail.com>
2019-07-31 16:21:50 +0200

    Switch to Ubuntu Xenial

--- .ci_scripts/package_appimage.sh
+++ .ci_scripts/package_appimage.sh
@@ -5,8 +5,7 @@ docker exec appimage /bin/bash -c "cd build
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 ldconfig -p | grep SDL
 
-# wget https://github.com/TheAssassin/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-centos6-x86_64.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 
 # This is supposed to be bundled in the previous AppImage but it's not right now. I manually download it as a workaround
 wget https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/linuxdeploy-plugin-appimage-x86_64.AppImage
@@ -14,6 +13,5 @@ wget https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/downloa
 chmod +x linuxdeploy*.AppImage
 
 export NO_CLEANUP=1 # Can be removed in the future, currently linuxdeploy has issues cleaning up things
-./linuxdeploy-centos6-x86_64.AppImage --appimage-extract-and-run --appdir AppDir
-./linuxdeploy-plugin-appimage-x86_64.AppImage --appimage-extract-and-run --appdir AppDir
+./linuxdeploy-x86_64.AppImage --appimage-extract-and-run --appdir AppDir --output appimage
 "
--- .ci_scripts/start_docker.sh
+++ .ci_scripts/start_docker.sh
@@ -2,10 +2,9 @@
 
 case "$BUILD_TARGET" in
 "appimage")
-    # At the time of writing this code this image corresponds to CentOS 6
+    # At the time of writing this code this image corresponds to Ubuntu Xenial (16.04 LTS)
     # The idea of AppImage is that you have to build on the oldest distribution that you want to support
-    # https://github.com/AppImage/AppImageKit/blob/ee71440ded7c450decb5643fbcfcc9e4c618ec7b/.travis.yml#L9
-	docker run -d --name appimage --device /dev/fuse:mrw --workdir /build/git -v "${PWD}:/build/git" quay.io/appimage/appimagebuild tail -f /dev/null
+	docker run -d --name appimage --device /dev/fuse:mrw --workdir /build/git -v "${PWD}:/build/git" ubuntu/xenial tail -f /dev/null
 	;;
 "vita")
 	docker run -d --name vitasdk --workdir /build/git -v "${PWD}:/build/git" gnuton/vitasdk-docker tail -f /dev/null
