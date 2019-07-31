505226a7dc34ebc10cfb0b142d0bfed8184fbbf1
devnoname120 <devnoname120+gh@gmail.com>
2019-07-31 16:24:57 +0200

    Fix docker tag

--- .ci_scripts/start_docker.sh
+++ .ci_scripts/start_docker.sh
@@ -4,7 +4,7 @@ case "$BUILD_TARGET" in
 "appimage")
     # At the time of writing this code this image corresponds to Ubuntu Xenial (16.04 LTS)
     # The idea of AppImage is that you have to build on the oldest distribution that you want to support
-	docker run -d --name appimage --device /dev/fuse:mrw --workdir /build/git -v "${PWD}:/build/git" ubuntu/xenial tail -f /dev/null
+	docker run -d --name appimage --device /dev/fuse:mrw --workdir /build/git -v "${PWD}:/build/git" ubuntu:xenial tail -f /dev/null
 	;;
 "vita")
 	docker run -d --name vitasdk --workdir /build/git -v "${PWD}:/build/git" gnuton/vitasdk-docker tail -f /dev/null
