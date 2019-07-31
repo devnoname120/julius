cc9ee3b6dfc605ed42d7126c5032cf11b3b935e6
devnoname120 <devnoname120+gh@gmail.com>
2019-07-31 20:50:25 +0200

    Add desktop file option to AppImage

--- .ci_scripts/package_appimage.sh
+++ .ci_scripts/package_appimage.sh
@@ -8,5 +8,5 @@ ldconfig -p | grep SDL
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
 
-./linuxdeploy-x86_64.AppImage --appimage-extract-and-run --appdir AppDir --output appimage
+./linuxdeploy-x86_64.AppImage --desktop-file appdir/usr/share/applications/julius.desktop --appdir AppDir --output appimage
 
